bc9a371929f567979e2b049bbc1d5bcb551a9238
Guillaume Boddaert <gboddaert@solocal.com>
2022-12-03 10:27:46 +0100

    feat: solved day03 second star

--- day03/__init__.py
+++ day03/__init__.py
@@ -23,22 +23,22 @@ class Compartment(list):
         self.raw = content
         super().__init__(sorted([Item(letter) for letter in content]))
 
+    @property
+    def set(self):
+        return set(self)
 
 class Rucksack:
     def __init__(self, content):
         p = int(len(content) / 2)
         self.comp1 = Compartment(content[:p])
         self.comp2 = Compartment(content[p:])
+        self.all = Compartment(content)
 
     @property
     def both(self):
-        a = set(self.comp1)
-        b = set(self.comp2)
-        c = a.intersection(b)
-        d = list(c)
-        return d
+        return list(self.comp1.set.intersection(self.comp2.set))
 
-def parse_priority(filename):
+def parse_overall_priority(filename):
     score = 0
     with open(filename, encoding="utf-8") as file:
         for line in file:
@@ -46,6 +46,20 @@ def parse_priority(filename):
             for item in rucksack.both:
                 score += item.value
     return score
+
+def parse_groups(filename):
+    score = 0
+    with open(filename, encoding="utf-8") as file:
+        for line in file:
+            elf1 = Rucksack(line.strip()).all.set
+            elf2 = Rucksack(next(file).strip()).all.set
+            elf3 = Rucksack(next(file).strip()).all.set
+
+            for common in elf1.intersection(elf2).intersection(elf3):
+                score += common.value
+
+    return score
+
 
 assert Item("a").value == 1
 assert Item("z").value == 26
@@ -57,7 +71,11 @@ assert s.comp1.raw == "vJrwpWtwJgWr"
 assert s.comp2.raw == "hcsFMMfFFhFp"
 assert s.both[0] == Item("p")
 
-assert parse_priority("test.txt") == 157
+assert parse_overall_priority("test.txt") == 157
+assert parse_groups("test2.txt") == 70
 
-result = parse_priority("input.txt")
+result = parse_overall_priority("input.txt")
 print(f"Total priority for item present in both rucksack is : {result}")
+
+result = parse_groups("input.txt")
+print(f"Total priority for common item present in 3 elfs groups is : {result}")
