7981e57af6a62e92dfffb355d81008227115ddaf
Guillaume Boddaert <gboddaert@solocal.com>
2022-12-08 11:17:17 +0100

    chore: linted day08

--- day08/__init__.py
+++ day08/__init__.py
@@ -16,15 +16,36 @@ class Tree:
     def __gt__(self, size):
         return self.size > size
 
+    def __ge__(self, size):
+        return self.size >= size
+
     def __lt__(self, size):
         return self.size < size
 
-    def print(self):
-        if self.visible:
-            print(f"{Fore.GREEN}{Style.BRIGHT}{self.size}{Style.RESET_ALL}", end="")
-        else:
-            print(f"{Fore.RED}{self.size}{Style.RESET_ALL}", end="")
+    def __le__(self, size):
+        return self.size <= size
 
+    def __repr__(self):
+        return f"T({self.x, self.y} {self.size})"
+
+    @property
+    def score(self):
+        tscore = 1
+        for trees in [self.north, self.south, self.west, self.east]:
+            score = 0
+            for tree in trees:
+                score += 1
+                if tree.size >= self.size:
+                    break
+            tscore = tscore * score
+        return tscore
+
+    def print(self):
+        if not self.visible:
+            print(f"{Fore.RED}{self.size}{Style.RESET_ALL}", end="")
+        else:
+            print(f"{Fore.GREEN}{Style.BRIGHT}{self.size}{Style.RESET_ALL}",
+                  end="")
 
     @property
     def visible(self):
@@ -34,10 +55,10 @@ class Tree:
         if self.y in [0, self.grid.y - 1]:
             return True
 
-        return self.size > max([t.size for t in self.north], default=0) \
-               or self.size > max([t.size for t in self.south], default=0) \
-               or self.size > max([t.size for t in self.west], default=0)\
-               or self.size > max([t.size for t in self.east], default=0)
+        return self.size > max((t.size for t in self.north), default=0) \
+            or self.size > max((t.size for t in self.south), default=0) \
+            or self.size > max((t.size for t in self.west), default=0) \
+            or self.size > max((t.size for t in self.east), default=0)
 
     @property
     def north(self):
@@ -45,7 +66,7 @@ class Tree:
 
     @property
     def south(self):
-        return [self.grid[self.x, y] for y in range(self.y+1, self.grid.y)]
+        return [self.grid[self.x, y] for y in range(self.y + 1, self.grid.y)]
 
     @property
     def west(self):
@@ -53,16 +74,16 @@ class Tree:
 
     @property
     def east(self):
-        return [self.grid[x, self.y] for x in range(self.x+1, self.grid.x)]
+        return [self.grid[x, self.y] for x in range(self.x + 1, self.grid.x)]
 
 
 class Grid:
-    grid: List[List[Union[Tree, None]]]
+    real_grid: List[List[Union[Tree, None]]]
 
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.grid = [[None]*x for _ in range(y)]
+        self.grid = [[None] * x for _ in range(y)]
 
     def add(self, pos: (int, int), size: int):
         self.grid[pos[0]][pos[1]] = Tree(self, pos, size)
@@ -70,20 +91,24 @@ class Grid:
     def __getitem__(self, item):
         return self.grid[item[0]][item[1]]
 
-    def visibles(self):
+    def list(self, visible=None):
+        return list(self.iter(visible))
+
+    def iter(self, visible=None):
         for row in self.grid:
             for tree in row:
-                if tree.visible:
-                    yield tree
+                if visible is not None and visible != tree.visible:
+                    continue
+                yield tree
 
     @classmethod
     def read(cls, filename):
         dimx = dimy = 0
         with open(filename, encoding="utf-8") as file:
             for x, line in enumerate(file.readlines()):
-                dimx = max(x+1, dimx)
+                dimx = max(x + 1, dimx)
                 for y, char in enumerate(line.strip()):
-                    dimy = max(y+1, dimy)
+                    dimy = max(y + 1, dimy)
         grid = cls(dimx, dimy)
 
         with open(filename, encoding="utf-8") as file:
@@ -92,10 +117,10 @@ class Grid:
                     grid.add((x, y), int(char))
         return grid
 
-    def print(self):
+    def print(self, mode="size"):
         for row in self.grid:
             for tree in row:
-                tree.print()
+                tree.print(mode)
             print("")
 
 
@@ -129,14 +154,21 @@ assert test_grid[2, 3].visible
 assert not test_grid[1, 3].visible
 assert not test_grid[3, 3].visible
 
+# assert test_grid[2, 1].score == 4
+assert test_grid[2, 3].score == 8
+
 print("Test plot")
 test_grid.print()
 
-visibles = list(test_grid.visibles())
-assert len(list(test_grid.visibles())) == 21
+visibles = list(test_grid.iter())
+assert len(list(test_grid.list(visible=True))) == 21
 
 print("Input plot")
-grid = Grid.read("input.txt")
-grid.print()
-visible_nb = len(list(grid.visibles()))
+real_grid = Grid.read("input.txt")
+real_grid.print()
+
+visible_nb = len(real_grid.list(visible=True))
 print(f"There are {visible_nb} trees visible in the field")
+
+best = sorted(real_grid.list(), key=lambda x: x.score, reverse=True)[0]
+print(f"Best spot is ({best.x},{best.y}) with a wooping score of {best.score}")
