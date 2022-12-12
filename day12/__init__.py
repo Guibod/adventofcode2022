800f1cfbec1948fdc8a9f2c8f4d01b1ac8e166e3
Guillaume Boddaert <gboddaert@solocal.com>
2022-12-12 14:49:46 +0100

    feat: day12 with astar but it is not correct

--- day12/__init__.py
+++ day12/__init__.py
@@ -1,10 +1,12 @@
 import logging
+import math
 from functools import lru_cache
-from typing import Dict, Iterator, List, Set
 from itertools import product
+from typing import Dict, Iterator
+from astar import AStar, Infinite
+from colorama import Fore, Style, Back
 
-
-RUN_TEST = False
+RUN_TEST = True
 DEBUG = True
 
 logger = logging.getLogger(__name__)
@@ -13,76 +15,47 @@ if DEBUG:
     logger.setLevel(logging.DEBUG)
 
 
-class DeathTrapException(Exception):
-    pass
-
-class Climber:
-    def __init__(self, map: 'Map', pos: (int, int) = None):
+class Climber(AStar):
+    def __init__(self, map: 'Map'):
+        super().__init__()
         self.max_positive = 1
         self.max_negative = float('-inf')
         self.map = map
-        self.history: List[(int, int)] = [pos or map.start.pos]
 
     def __repr__(self):
-        return f"Climber at {self.pos}"
+        return "A-Star Climber"
 
-    @property
-    def moves(self) -> int:
-        return len(self.history) - 1
+    def heuristic_cost_estimate(self, n1: 'Square', n2: 'Square'):
+        """computes the 'direct' distance between two (x,y) tuples"""
+        (x1, y1) = n1.pos
+        (x2, y2) = n2.pos
+        return math.hypot(x2 - x1, y2 - y1)
 
-    @property
-    def pos(self):
-        return self.history[-1]
+    def distance_between(self, n1: 'Square', n2: 'Square'):
+        """this method always returns 1, as two 'neighbors' are always adajcent"""
+        if n2.elevation - n1.elevation > 1:
+            return Infinite
 
-    def move_to(self, target):
-        logger.debug("%s moves to : %s", self, target)
-        self.history.append(target.pos)
+        return 1
 
-    def plan_to(self, target, ahead=5) -> List[Square]:
-        logger.debug("%s evaluate %d next moves toward %s", self, ahead, target)
-        moves = []
-        while len(moves) < ahead:
-            options = self.options(self.pos, target)
-            if len(options) == 0:
-                raise DeathTrapException()
+    def neighbors(self, node: 'Square'):
+        return node.neighbours
+
+    def solve(self, source=None, target=None):
+        if not source:
+            source = self.map.start
+        if not target:
+            target = self.map.top
+
+        return list(self.astar(source, target))
 
 
-    def walk_to(self, target, max=100):
-        i = 0
-        logger.debug("%s starts a walk toward %s", self, target)
-        while self.pos != target.pos:
-            i += 1
-            if i >= max:
-                print(self.history)
-                raise RuntimeError(f"Climber is exhausted after {max} moves")
-            self.move_to(target)
+class Climber2(Climber):
+    def __repr__(self):
+        return "A-star with no Heuristic Cost Climber"
 
-        logger.debug("%s has reached his goal", self)
-
-    def can_climb(self, diff):
-        return self.max_negative <= diff <= self.max_positive
-
-    def accessible(self, square: 'Square'):
-        for neighbour in square.neighbours():
-            diff = neighbour.elevation - square.elevation
-            if neighbour.pos[0] != square.pos[0] and neighbour.pos[1] != square.pos[1]:
-                # Diagonal
-                continue
-
-            if self.can_climb(diff):
-                yield neighbour
-
-    def options(self, source: (int, int), target: (int, int)):
-        if source == target:
-            return [target.pos]
-
-        options = []
-        for option in self.accessible(self.map[source]):
-            if option.pos in self.history:
-                continue  # cannot go back
-            options.append(option)
-
-        return sorted(options, key=lambda x: (x.distance(target), x.height_difference(target)))
+    def heuristic_cost_estimate(self, n1: 'Square', n2: 'Square'):
+        return 1
 
 
 class Square:
@@ -95,16 +68,13 @@ class Square:
     def __hash__(self):
         return hash(self.pos)
 
+    def map_point(self, path):
+        if self in path:
+            return Back.BLACK + chr(self.elevation + 97) + Style.RESET_ALL
+        return self.map.print_color(self.elevation) + chr(self.elevation + 97) + Style.RESET_ALL
+
     def distance(self, other: 'Square'):
         return max(abs(other.pos[0] - self.pos[0]), abs(other.pos[1] - self.pos[1]))
-
-    def height_difference(self, other: 'Square'):
-        return other.elevation - self.elevation
-
-    def
-
-    def is_deathtrap_for(self, other: 'Square', climber: Climber):
-        pass
 
     def __repr__(self):
         return f"Square {self.pos} [{self.elevation}]"
@@ -113,13 +83,20 @@ class Square:
     @lru_cache
     def neighbours_pos(self):
         x, y = self.pos
-        x_s = [x for x in range(x - 1, x + 2) if 0 <= x < self.map.width]
-        y_s = [y for y in range(y - 1, y + 2) if 0 <= y < self.map.height]
-        return [pos for pos in product(x_s, y_s) if pos != self.pos]
+        north = (x, y-1)
+        south = (x, y+1)
+        west = (x-1, y)
+        east = (x+1, y)
 
+        return [south, north, west, east]
+
+    @property
     def neighbours(self) -> Iterator['Square']:
         for pos in self.neighbours_pos:
-            yield self.map[pos]
+            try:
+                yield self.map[pos]
+            except KeyError:
+                pass
 
 
 class Map:
@@ -158,38 +135,65 @@ class Map:
 
         return parsed_map
 
+    @property
+    @lru_cache()
+    def gradient(self):
+        bcolor = [Back.WHITE, Back.GREEN, Back.YELLOW, Back.RED]
+        bstyle = [Style.DIM, Style.NORMAL, Style.BRIGHT]
+        fcolor = [Fore.BLACK]
+        fstyle = [Style.DIM, Style.NORMAL, Style.BRIGHT]
+        return list(product(bcolor, bstyle, fstyle, fcolor))
+
+    def print_color(self, elevation):
+        e = elevation
+        t = self.top.elevation
+        l = len(self.gradient)
+        try:
+            return "".join(self.gradient[math.floor(e * l / t)])
+        except IndexError:
+            return ""
+
+    def print(self, path):
+        for y in range(0, self.height):
+            for x in range(0, self.width):
+                print(self[x, y].map_point(path), end="")
+            print()
+
 
 if RUN_TEST:
     map = Map.parse("test.txt")
     climber = Climber(map)
-
-    assert climber.pos == map.start.pos
 
     assert map.height == 5
     assert map.width == 8
 
     assert map.start.elevation == 0
     assert map.start.pos == (0, 0)
-    assert map.start.neighbours_pos == [(0, 1), (1, 0), (1, 1)]
-    assert [square.pos for square in climber.accessible(map.start)] == [(0, 1), (1, 0)]
+    assert list(map.start.neighbours) == [map[0, 1], map[1, 0]]
 
     assert map.top.elevation == 26
     assert map.top.pos == (5, 2)
-    assert map.top.neighbours_pos == [(4, 1), (4, 2), (4, 3), (5, 1), (5, 3), (6, 1), (6, 2), (6, 3)]
-    assert len(list(climber.accessible(map.top))) == 4
+    assert list(map.top.neighbours) == [map[5, 3], map[5, 1], map[4, 2], map[6, 2]]
 
-    assert list(climber.accessible(map[2, 2])) == [map[1, 2], map[2, 1], map[2, 3]]
+    path = climber.solve()
+    assert len(path) == 32
+    assert path[0] == map[(0, 0)]
+    assert path[1] == map[(1, 0)]
+    assert path[2] == map[(2, 0)]
+    assert path[-2] == map[(4, 2)]
+    assert path[-1] == map[(5, 2)]
 
-    assert climber.options(map.top) == [map[1, 0], map[0, 1]]
-    assert climber.options(map[(1, 0)]) == [map[1, 0], map[0, 1]]
-
-    climber.walk_to(map.top)
-    assert climber.moves == 31
+    assert list(map[0, 3].neighbours) == [map[0, 4], map[0, 2], map[1, 3]]
 
 map = Map.parse("input.txt")
-climber2 = Climber(map, (12, 10))
-print(climber2.options(map.top))
 
 climber = Climber(map)
-climber.walk_to(map.top)
-print(f"The climber moved {climber.moves}, to reach {map.top}")
+path = set(climber.solve())
+map.print(path)
+print(f"{climber} moved {len(path)-1} squares, to reach {map.top}")
+
+climber2 = Climber2(map)
+path3 = set(climber2.solve())
+map.print(path3)
+print(f"{climber2} moved {len(set(path3))-1} squares, to reach {map.top}")
+
